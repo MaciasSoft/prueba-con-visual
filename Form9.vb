5318b5462d162fcc23c13d583f7cb1084cea659d
unknown <maciasnelsonti@gmail.com>
2018-10-04 16:29:57 -0400

    prueba de subida

---
+++ Form9.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form9
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form9_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label16_Click(sender As Object, e As EventArgs) Handles Label16.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Hide()
+
+    End Sub
+End Class
